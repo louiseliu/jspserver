24be6c5ea10b85d59326bd75a53df88aad5b7576
liuguolin <guolin.liu@duobei.com>
2013-08-09 14:46:21 +0800

    init

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views/');
+app.set('view engine', 'jade');
+app.engine('jsp',require('./lib/endjava').__express);
+
+app.get('/test',function(req,res)
+{
+    //now use endskin to parse index.html!  the second parameter is the data object
+    res.render('test.jsp',{user:"dsfsdfsdf"});
+    
+});
+
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ views/test.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>test.jsp</title>
+</head>
+<body>
+<p>user: ${user}</p>
+<c:if test = "${user}">
+dsfsdfjsldf
+</c:if>
+</body>
+</html>
